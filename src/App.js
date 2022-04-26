ff5bcb417b6fddb1e3aa24946b9886aa361a51ae
AwesomeWithRex <97481247+AwesomeWithRex@users.noreply.github.com>
2022-04-26 13:30:18 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Cat from './Cat';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Cat />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+function Header() {
+    return (
+        <div className="header__main">
+            <h1>Catz 😺</h1>
+        </div>
+    )
+}
+
+export default Header
